049ca118814e863db686490fa3171087317d281f
James Turner <stsparrow@users.noreply.github.com>
2020-06-24 16:22:28 -0600

    Update build.sh
    Updated to support Nodejs 10, eliminated linux x64 and windows builds

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ cp new-package.json node_modules/node-red/package.json
   cd node_modules/node-red || exit 1
   npm install --production
   pkg \
-    --targets node8-linux-armv7,node8-linux-x64,node8-win-x64,node8-macos-x64 \
+    --targets node10-linux-armv7,node10-win-x64, \
     --out-path ../../release --public .
 )
