83b23eab4c12bc25f934648b9c04651c4e9abe69
James Turner <stsparrow@users.noreply.github.com>
2020-06-25 11:10:50 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ cp new-package.json node_modules/node-red/package.json
   cd node_modules/node-red || exit 1
   npm install --production
   pkg \
-    --targets node8-linux-armv7,node8-linux-x64,node8-win-x64,node8-macos-x64,node10-linux-armv7,node10-linux-x64,node10-win-x64,node10-macos-x64 \
+    --targets node8.16.0-linux-armv7,node8.16.0-linux-x64,node8.16.0-win-x64,node8.16.0-macos-x64,node10.15.3-linux-armv7,node10.15.3-linux-x64,node10.15.3-win-x64,node10.15.3-macos-x64 \
     --out-path ../../release --public .
 )
