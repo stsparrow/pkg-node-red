2445b24beb2e5008d0887c6772bc6caf5f9bd3f5
James Turner <stsparrow@users.noreply.github.com>
2020-06-25 10:01:40 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-
+npm install bcryptjs -g --unsafe-perm --build-from-source
 npm install "node-red@$NODERED_VERSION"
 node -e "const data = require('./node_modules/node-red/package.json'); data.pkg = { assets: ['./**/*'] }; require('fs').writeFileSync('new-package.json', JSON.stringify(data, null, ' '));"
 cp new-package.json node_modules/node-red/package.json
@@ -9,6 +9,6 @@ cp new-package.json node_modules/node-red/package.json
   cd node_modules/node-red || exit 1
   npm install --production
   pkg \
-    --targets node10-linux-armv7, \
+    --targets node12.2.0-linux-armv7 \
     --out-path ../../release --public .
 )
